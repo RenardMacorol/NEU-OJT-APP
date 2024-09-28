bcc75b04bee6bc2b1ae62791b3e8886049abdff5
Jaime Dy III <152839721+JaimeDyIII@users.noreply.github.com>
2024-09-28 11:52:20 +0800

    Update App.tsx

--- ojt/src/App.tsx
+++ ojt/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Login from './components/Login';
+import MainOptions from "./components/MainOptions";
+import SearchBar from "./components/SearchBar";
+import Header from "./components/Header";
+
 const App: React.FC = () => {
   return (
-    <div>
-      <Login />
+    <div className = "app-container">
+      <Header />
+      <SearchBar />
+      <MainOptions />
     </div>
   );
 }
