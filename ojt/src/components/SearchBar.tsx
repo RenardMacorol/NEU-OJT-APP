79454d6d23dc42d047848391453239bf4bf3482f
Jaime Dy III <152839721+JaimeDyIII@users.noreply.github.com>
2024-09-29 20:44:46 +0800

    Update SearchBar.tsx

--- ojt/src/components/SearchBar.tsx
+++ ojt/src/components/SearchBar.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import React from "react";
 
@@ -10,17 +9,4 @@ const SearchBar: React.FC = () => {
     )
 }
 
-=======
-import "./App.css";
-import React from "react";
-
-const SearchBar = () => {
-    return(
-      <div className="search-bar">
-        <input type="text" placeholder="Search" className="search-bar"/>
-      </div>
-    )
-}
-
->>>>>>> bcc75b04bee6bc2b1ae62791b3e8886049abdff5
 export default SearchBar;
