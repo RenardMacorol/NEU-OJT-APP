f6cd2b696e46d8c38b8ce4cc4a3a22e3aae8f66f
Jaime Dy III <152839721+JaimeDyIII@users.noreply.github.com>
2024-09-29 20:43:39 +0800

    Update Header.tsx

--- ojt/src/components/Header.tsx
+++ ojt/src/components/Header.tsx
@@ -13,20 +13,4 @@ const Header: React.FC = () => {
     )
 }
 
-=======
-import "./App.css";
-import React from "react";
-import AppName from "./AppName";
-import Logout from "./Logout";
-
-const Header = () => {
-    return (
-           <div className = 'header'>
-                <AppName />
-                <Logout />
-           </div>
-    )
-}
-
->>>>>>> bcc75b04bee6bc2b1ae62791b3e8886049abdff5
 export default Header;
