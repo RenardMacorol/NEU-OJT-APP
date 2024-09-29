d1e978519a723ff849352575e634cb6169c2072a
Jaime Dy III <152839721+JaimeDyIII@users.noreply.github.com>
2024-09-29 20:43:18 +0800

    Update AppName.tsx
    Fixing some bugs

--- ojt/src/components/AppName.tsx
+++ ojt/src/components/AppName.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import React from "react";
 
@@ -7,16 +6,5 @@ const AppName: React.FC = () => {
             <h1 className = 'app-name'>NEU's OJT APP</h1>
     )
 }
-
-=======
-import "./App.css";
-import React from "react";
-
-const AppName = () => {
-    return (
-            <h1 className = 'app-name'>NEU's OJT APP</h1>
-    )
-}
-
->>>>>>> bcc75b04bee6bc2b1ae62791b3e8886049abdff5
+    
 export default AppName;
