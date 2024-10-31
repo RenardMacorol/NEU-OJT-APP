8ff8ae39ec974f6c9e4f2d08491a7def0ed5a073
jdy <dyiiijaime@gmail.com>
2024-10-31 23:24:13 +0800

    Removed sidebar

--- ojt/src/pages/Dashboard.tsx
+++ ojt/src/pages/Dashboard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MainOptions from "../components/MainOptions";
 import Header from "../components/Header";
-import Sidebar from '../components/Sidebar';
 import "../styles/App.css"
 
 const Dashboard: React.FC = () => {
@@ -9,7 +8,6 @@ const Dashboard: React.FC = () => {
     <div className = "app-container bg-gradient-to-t from-teal-500 to-blue-900 min-h-screen w-full items-center justify-center p-2.5 box-border overflow-hidden">
       <Header />
       <MainOptions />
-      <Sidebar />
     </div>
   );
 }
